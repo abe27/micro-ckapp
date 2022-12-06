9f80014a8993617b5d1723ad8a88a097c545212e
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-06 13:33:10 +0700

    show stock

--- pages/stock/index.js
+++ pages/stock/index.js
@@ -10,11 +10,46 @@ import {
 } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { Spinner } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ReDateTime } from "../../hooks/greeter";
 
 const StockPage = () => {
   const { data: session } = useSession();
   const [isLoading, setIsLoading] = useState(false);
+  const [stocks, setStocks] = useState([]);
+  const [txtPartFilter, setTxtPartFilter] = useState("");
+
+  const FetchData = async () => {
+    setIsLoading(true);
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", session?.user.accessToken);
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+    let host = `${process.env.API_HOST}/stock?tag=C`
+    if (txtPartFilter !== "") {
+      host = `${process.env.API_HOST}/stock?part_no=${txtPartFilter}&tag=C`
+    }
+    console.log(host)
+    const res = await fetch(host,requestOptions);
+
+    if (res.ok) {
+      const data = await res.json();
+      console.dir(data.data);
+      setStocks(data.data);
+      setTxtPartFilter("")
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(()=> {
+    if (session?.user) {
+      FetchData();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session?.user])
   return (
     <>
       <NavBar
@@ -24,16 +59,21 @@ const StockPage = () => {
       />
       <section className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="lg:flex lg:items-center lg:justify-between">
-          <nav className="flex-1 min-w-0">
-            <strong className="text-gray-900 tfont-bold">
+          <nav className="flex justify-start flex-1 min-w-0">
+            <section className="font-bold text-gray-900">
               <div className="form-control">
                 <div className="input-group input-group-sm">
                   <input
                     type="text"
                     placeholder="ระบุเลขที่สินค้า"
                     className="input input-bordered input-sm"
+                    value={txtPartFilter}
+                    onChange={(e) => setTxtPartFilter(e.target.value)}
                   />
-                  <button className="btn btn-square btn-sm">
+                  <button
+                    className="btn btn-square btn-sm"
+                    onClick={FetchData}
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6"
@@ -51,90 +91,14 @@ const StockPage = () => {
                   </button>
                 </div>
               </div>
-            </strong>
-            {/* <div className="flex flex-col mt-1 sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-              <div className="flex items-center mt-2 text-sm text-gray-500">
-                <BriefcaseIcon
-                  className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                  aria-hidden="true"
-                />
-                <Link href={`/order/archive`}>Archive</Link>
-              </div>
-              <div className="flex items-center mt-2 text-sm text-gray-500">
-                <CloudIcon
-                  className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                  aria-hidden="true"
-                />
-                <Link href={`/order/edi`}>Remote EDI</Link>
-              </div>
-              <div className="flex items-center mt-2 text-sm text-gray-500">
-                <CalendarIcon
-                  className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400 hover:cursor-pointer"
-                  aria-hidden="true"
-                />
-                <span>Filter on </span>
-              </div>
-            </div> */}
+            </section>
           </nav>
           <nav className="flex mt-5 lg:mt-0 lg:ml-4">
-            {session?.user.isAdmin ? (
-              <span className="hidden ml-3 sm:block">
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm font-small hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <PencilIcon
-                    className="w-5 h-5 mr-2 -ml-1 text-gray-500"
-                    aria-hidden="true"
-                  />
-                  ปิดแก้ไข
-                </button>
-              </span>
-            ) : (
-              <></>
-            )}
-            <span className="hidden ml-3 sm:block">
-              <Link
-                href={`/order/joblist?id=`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <PrinterIcon
-                    className="w-5 h-5 mr-2 -ml-1"
-                    aria-hidden="true"
-                  />
-                  JobList
-                </button>
-              </Link>
-            </span>
-            <span className="hidden ml-3 sm:block">
-              <Link
-                href={`/order/pallet?id=`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-orange-600 border border-transparent rounded-md shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-                >
-                  <PrinterIcon
-                    className="w-5 h-5 mr-2 -ml-1"
-                    aria-hidden="true"
-                  />
-                  Dimension
-                </button>
-              </Link>
-            </span>
-
             <span className="sm:ml-3">
               <button
                 type="button"
                 className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={() => FetchOrder()}
+                onClick={() => FetchData()}
               >
                 {isLoading ? (
                   <Spinner size={`sm`} />
@@ -149,7 +113,65 @@ const StockPage = () => {
             </span>
           </nav>
         </div>
-        <div className="divider" /> 
+        <div className="divider" />
+        <div className="mt-4 overflow-x-auto">
+          <table className="table w-full table-compact">
+            <thead>
+              <tr>
+                <th></th>
+                <th>สินค้า</th>
+                <th></th>
+                <th>เลขที่ LotNo.</th>
+                <th></th>
+                <th></th>
+                <th>จำนวน</th>
+                <th>กล่อง</th>
+                <th>ชั้น</th>
+                <th>พาเลท</th>
+                <th>อัพเดทล่าสุด</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stocks &&
+                stocks.map((i) => (
+                  <tr key={i.id} className="hover hover:text-sky-600">
+                    <th>{i.id}</th>
+                    <td>
+                      <Link
+                        href={`/stock/detail?part_no=${i.part_no}&tag=${i.tagrp}`}
+                      >
+                        {i.part_no}
+                      </Link>
+                    </td>
+                    <td>{i.part_name}</td>
+                    <td>{i.lot_no}</td>
+                    <td>{i.line_no}</td>
+                    <td>{i.revise_no}</td>
+                    <td>{(i.qty * i.ctn).toLocaleString()}</td>
+                    <td>{i.ctn.toLocaleString()}</td>
+                    <td>{i.shelve}</td>
+                    <td>{i.pallet_no}</td>
+                    <td>{ReDateTime(i.updated_at)}</td>
+                  </tr>
+                ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th></th>
+                <th>สินค้า</th>
+                <th></th>
+                <th>เลขที่ LotNo.</th>
+                <th></th>
+                <th></th>
+                <th>จำนวน</th>
+                <th>กล่อง</th>
+                <th>ชั้น</th>
+                <th>พาเลท</th>
+                <th>อัพเดทล่าสุด</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </section>
     </>
   );
